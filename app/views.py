575b02d9c1378460a0476af19b74d7a4c129b71f
Fikri <fikrizhanking@gmail.com>
2020-12-18 10:52:31 +0700

    Update views & search

--- app/views.py
+++ app/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 
-from bs4 import BeautifulSoup
 import requests
+from requests.compat import quote_plus 
+from bs4 import BeautifulSoup
+from . import models
 
-# Create your views here.
+CRAIGLIST_URL = 'https://newyork.craiglist.org/search/?query={}'
+
 def home(request):
 	return render(request, 'base.html')
 
 def new_search(request):
+	search = request.POST.get('search')
+	url = CRAIGLIST_URL.format(quote_plus(search))
+	# response = requests.get(url)
+	r = ('c', 'a', 'd')
 	data = {
-		'search': request.POST.get('search')
+		'search': r
 	}
 	return render(request, 'app/new_search.html', data)
