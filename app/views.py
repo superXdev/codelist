08d000b63f3f67a28eda7a842a4e7b3823ef36cb
Fikri Rudiansyah <fikrizhanking@gmail.com>
2020-12-18 13:22:05 +0700

    Adding scrapping to views & update search template

--- app/views.py
+++ app/views.py
@@ -5,17 +5,42 @@ from requests.compat import quote_plus
 from bs4 import BeautifulSoup
 from . import models
 
-CRAIGLIST_URL = 'https://newyork.craiglist.org/search/?query={}'
+CRAIGLIST_URL = 'https://hongkong.craigslist.org/search/sss?query={}&sort=rel'
+IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 def home(request):
 	return render(request, 'base.html')
 
 def new_search(request):
+	# get keyword search
 	search = request.POST.get('search')
+	# getting the webpage
 	url = CRAIGLIST_URL.format(quote_plus(search))
-	# response = requests.get(url)
-	r = ('c', 'a', 'd')
+	response = requests.get(url)
+	# passing the source code
+	soup = BeautifulSoup(response.text, features='html.parser')
+	# extract all post
+	post_lists = soup.find_all('li', {'class': 'result-row'})
+
+	result_posts = []
+	for post in post_lists:
+		title = post.find(class_='result-title').text
+		url = post.find('a').get('href')
+		price = 'N/A'
+		image_url = 'https://stickershop.line-scdn.net/stickershop/v1/product/1000415/LINEStorePC/main.png;compress=true'
+
+		if post.find(class_='result-price'):
+			price = post.find(class_='result-price').text
+
+		if post.find(class_='result-image'):
+			ids = post.find(class_='result-image').get('data-ids')
+			image_url = ids.split(",")[0].split(":")[1]
+			image_url = IMAGE_URL.format(image_url)
+
+		result_posts.append((title, url, price, image_url))
+
 	data = {
-		'search': r
+		'search': search,
+		'posts': result_posts,
 	}
 	return render(request, 'app/new_search.html', data)
